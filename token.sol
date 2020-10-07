676afc5ba0d6bc2b992237d2f272feacfc2cb438
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-10-06 19:58:53 -0700

    first commit

---
+++ token.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.7.0;
+
+// SPDX-License-Identifier: MIT
+
+import "./SafeMath.sol";
+
+contract Token {
+    function totalSupply() constant return (uint256 supply) {}
+
+    function balanceOf (address _owner) constant returns (uint256  )
+}
